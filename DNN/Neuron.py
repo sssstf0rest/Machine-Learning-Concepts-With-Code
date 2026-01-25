bad64093b4e78a3a01ac1649f08eb2f595e6b196
Haoshen <haoshen0226@gmail.com>
2026-01-25 16:37:09 +0800

    update CNN with matrix multiplication

--- DNN/Neuron.py
+++ DNN/Neuron.py
@@ -4,11 +4,17 @@ import numpy as np
 class Layer_Dense:
 
     # Layer initialization
-    def __init__(self, n_inputs, n_neurons, name=None):
+    def __init__(self, n_inputs, n_neurons,
+                 weight_regularizer_l1=0, weight_regularizer_l2=0,
+                 bias_regularizer_l1=0, bias_regularizer_l2=0):
         # Initialize weights and biases
-        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-        self.name = name or f"Dense({n_inputs}->{n_neurons})"
+        # Set regularization strength
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
 
     # Forward pass
     def forward(self, inputs):
@@ -16,63 +22,31 @@ class Layer_Dense:
         self.inputs = inputs
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
-
     # Backward pass
     def backward(self, dvalues):
         # Gradients on parameters
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
-        
+
+        # Gradients on regularization
+        # L1 on weights
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+        # L2 on weights
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * \
+                             self.weights
+        # L1 on biases
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+        # L2 on biases
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * \
+                            self.biases
+
         # Gradient on values
         self.dinputs = np.dot(dvalues, self.weights.T)
-        
-    @property
-    def in_dim(self):
-        return self.weights.shape[0]
-
-    @property
-    def out_dim(self):
-        return self.weights.shape[1]
-
-    def summary(self):
-        params = self.weights.size + self.biases.size
-        return {
-            "type": "Dense",
-            "name": self.name,
-            "in": self.in_dim,
-            "out": self.out_dim,
-            "params": params,
-        }
-
-# class Layer_Dense:
-#     def __init__(self, n_inputs, n_neurons, name=None):
-#         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
-#         self.biases = np.zeros((1, n_neurons))
-#         self.name = name or f"Dense({n_inputs}->{n_neurons})"
-
-#     def forward(self, inputs):
-#         self.inputs = inputs
-#         self.output = np.dot(inputs, self.weights) + self.biases
-
-#     def backward(self, dvalues):
-#         self.dweights = np.dot(self.inputs.T, dvalues)
-#         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
-#         self.dinputs = np.dot(dvalues, self.weights.T)
-
-#     @property
-#     def in_dim(self):
-#         return self.weights.shape[0]
-
-#     @property
-#     def out_dim(self):
-#         return self.weights.shape[1]
-
-#     def summary(self):
-#         params = self.weights.size + self.biases.size
-#         return {
-#             "type": "Dense",
-#             "name": self.name,
-#             "in": self.in_dim,
-#             "out": self.out_dim,
-#             "params": params,
-#         }
